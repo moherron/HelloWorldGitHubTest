69fc335fd22cd115181bb976672adf4518f66d18
O'Herron <moherron@zagmail.gonzaga.edu>
2019-08-29 10:11:36 -0700

    insert hello again

--- src/Demo.java
+++ src/Demo.java
@@ -2,6 +2,7 @@ public class Demo {
     public static void main(String[] args){
         System.out.println("HELLO");
         System.out.println("GOODBYE");
+        System.out.println("HELLO AGAIN");
 
     }
 }
